e7287fe00407570ea0555efe3a0e7a53529096f4
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-07-06 15:05:26 +0200

    fix(python): Add missing state

--- 1_api_python/api/fpga_api.py
+++ 1_api_python/api/fpga_api.py
@@ -20,7 +20,7 @@ class FlashFPGA(DeviceAPI):
         super().__init__(com_name, timeout)
         self.__logger = getLogger(__name__)
         self.extend_pins_list(["FPGA_PWR_EN", "FPGA_PROGB", "FPGA_RSTN"])
-        self.extend_states_list(["ERASE_FLASH"])
+        self.extend_states_list(["FLASHING", "ERASE_FLASH"])
 
         self.__init_flash()
         self.__init_fpga()
